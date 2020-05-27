e22c5c9f1a2c14b5064d47b2220261c0563b90fd
OSG-GitHub <oceaniaservergroup@gmail.com>
2020-05-28 00:41:41 +1000

    Update Logger-Install.bat

--- Logger-Install.bat
+++ Logger-Install.bat
@@ -8,6 +8,35 @@ mkdir "%UserProfile%\Website" > NUL 2> NUL
 mkdir "%UserProfile%\Website\Serve" > NUL 2> NUL
 mkdir "%UserProfile%\Website\Logger" > NUL 2> NUL
 
+echo=
+echo=			     !! Note this reqires Python3 from the Microsoft Store !!
+echo=
+echo=				 If you have not yet installed Python3, do so now.
+echo=				           Press any key to begin install
+pause > NUL 2> NUL
+echo=
+echo=
+echo=
+echo=
+echo=
+echo=
+echo=
+echo=
+echo=                            ^|--------------------------------------------------------^|
+echo=		            ^|Downloading and Installing dependencies, please wait....^|
+echo=                            ^|--------------------------------------------------------^|
+echo=
+echo=
+echo=
+echo=
+echo=
+echo=
+echo=
+echo=
+echo=
+echo=
+
+
 PowerShell (New-Object System.Net.WebClient).DownloadFile('https://github.com/html5up/identity/archive/master.zip','%UserProfile%\Website\master.zip') > NUL 2> NUL
 PowerShell (New-Object System.Net.WebClient).DownloadFile('https://github.com/RyanTheTide/BasicIPGrabber_with_Python3/raw/master/assets/unzip.exe','%UserProfile%\Website\unzip.exe') > NUL 2> NUL
 "%UserProfile%\Website\unzip.exe" "%UserProfile%\Website\master.zip" -d "%UserProfile%\Website\master" > NUL 2> NUL
@@ -33,7 +62,9 @@ echo=python3 -m http.server 80 -d "%UserProfile%\Website\Serve" ^> "%UserProfile
 
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Desktop\Start Web Logger.lnk');$s.TargetPath='%userprofile%\Website\Logger-Start.bat';$s.Save()" > NUL 2> NUL	
 
-echo=Press any key to begin the website, you may wish to edit the "%UserProfile%\Website\Serve\index.html" with your details first though!
+echo=
+echo=		    Installed!! Press any key to close the installer and begin the logger,
+echo=	    you may wish to edit the "%UserProfile%\Website\Serve\index.html" with your details first though!
 pause > NUL 2> NUL
 
 start cmd /k "%UserProfile%\Website\Logger-Start.bat && exit"
